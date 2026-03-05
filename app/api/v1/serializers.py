2eed595dedd4509264650f58a9bda94b636f81b4
Vladyslav Buzhdyhan <82763244+vladbuzhdyhan@users.noreply.github.com>
2026-03-05 20:19:00 +0200

    feat: DS-21 add groups field to UserSerializer
    Add groups field to UserSerializer so frontend could save the group to
    the localstorage for rendering only necessary UI elements.

--- app/api/v1/serializers.py
+++ app/api/v1/serializers.py
@@ -1,11 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ["name"]
+
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(read_only=True, many=True)
+
     class Meta:
         model = get_user_model()
-        fields = ["id", "username", "email", "password"]
+        fields = ["id", "username", "email", "password", "groups"]
         extra_kwargs = {
             "password": {"write_only": True},
             "email": {"write_only": True},

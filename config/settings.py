ac420be0034df14f882938fb9975b14addc3be10
veronikas <syvaveronika@gmail.com>
2026-03-06 23:30:00 +0200

    chore(core):DS-20 make Emailservice URL configurable via env

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,7 @@ INITIAL_GROUPS = (
 BLOCKED_IP_REDIRECT_URL = "https://github.com/Deploy-Squad1"  # Change before deployment
 
 # URL of the Email microservice
-EMAIL_SERVICE_BASE_URL = "http://localhost:8081"
+EMAIL_SERVICE_BASE_URL = os.getenv("EMAIL_SERVICE_URL")
 
 # URL of the Map microservice
 MAP_SERVICE_BASE_URL = "http://localhost:8080"
